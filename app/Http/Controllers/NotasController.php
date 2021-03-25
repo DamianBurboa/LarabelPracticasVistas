63b54fffac476c7fc6b5a014d7c282511fc49497
unknown <you@example.com>
2021-03-25 06:20:19 -0700

    Practica

---
+++ app/Http/Controllers/NotasController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Notas;
+
+class NotasController extends Controller
+{
+    public function index(){
+        $notas = Notas::all(); //DB::table('notas')->get();
+    
+        return view('notas', ['notas' => $notas]);
+    }
+
+    public function agregar(){
+        return view('agregar');
+    }
+
+    public function store (Request $request){
+        Notas::create([
+            'titulo' => $request->input('title'),
+            'contenido' => $request->input('content'),
+        ]);
+    
+        return redirect('/notas');
+    
+    }
+
+    public function edit ($id){
+        $notas = Notas::find($id); //DB::table('notas')->where('id',$id)->first();
+        return view('editarnota', ['notas' => $notas]);
+    }
+
+    public function update(Notas $notas, Request $request){
+
+        $notas->update([
+            'titulo' => $request->input('title'),
+            'contenido' => $request->input('content'),
+        ]);
+
+        return redirect('/notas');
+    }
+
+    public function destroy($id){
+        $notas = Notas::find($id);
+        $notas->delete();
+
+        return redirect('/notas');
+    }
+}
